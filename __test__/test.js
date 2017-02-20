b649be9e8b5ac095ad5c2df14f0aeca95545f784
Michael Wuergler <wuergler@gmail.com>
2017-02-20 10:54:32 +0000

    add tests

---
+++ __test__/test.js
@@ -0,0 +1,30 @@
+const metalsmith = require('../metalsmith');
+const rm = require('rimraf');
+
+beforeEach(() => {
+  return rm.sync('build');
+});
+
+test('metalsmith builds', (done) => {
+  metalsmith.build(function(err, files) {
+      expect(err).toBe(null);
+      done();
+  });
+});
+
+test('index.html', (done) => {
+  metalsmith.build(function(err, files) {
+    const file = files['index.html'];
+    const expected = '<p>This is the content for your <b>Home</b> page.</p>';
+    const actual = file.contents.toString('utf8');
+
+    expect(err).toBe(null);
+    expect(file).toBeTruthy();
+    expect(actual.search(expected) > 0).toBe(true);
+    done();
+  });
+});
+
+afterAll(() => {
+  return rm.sync('build');
+});
